ee2cda90ce56533bb8920fc151b4bafe981c4185
Tanay2023 <202201026@daiict.ac.in>
2025-07-28 16:22:06 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const {Server} = require('socket.io');
+const mongoose = require('mongoose');
+const locationRoutes = require('./routes/locationRoutes');
+const { handleSocketConnection } = require('./socketHandlers');
+
+require('dotenv').config();
+
+const allowedOrigins = [process.env.FRONTEND_URL_DEV];
+
+const app = express();
+app.use(cors({
+  origin: allowedOrigins,     // allow your frontend origin
+  credentials: true
+}));
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",     // allow your frontend origin
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+
+});
+// Middleware
+app.use(express.json());
+
+app.use('/api/locations', locationRoutes);
+
+// MongoDB connection
+/*mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));*/
+
+// Socket.io connection
+io.on('connection', (socket) => {
+    console.log('New client connected');
+    handleSocketConnection(socket, io);
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
+// Start the server
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
