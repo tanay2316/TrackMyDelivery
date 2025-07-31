6c72fa7beacb87195b3d30290c453a6bd2351980
tanay2316 <146132092+tanay2316@users.noreply.github.com>
2025-07-31 15:15:07 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -31,13 +31,13 @@ app.use(express.json());
 
 app.use('/api/locations', locationRoutes);
 
-// // MongoDB connection
-// mongoose.connect(process.env.MONGO_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// })
-// .then(() => console.log('MongoDB connected'))
-// .catch(err => console.error('MongoDB connection error:', err));
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 // Socket.io connection
 io.on('connection', (socket) => {
